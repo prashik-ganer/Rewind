7cf7d6e17eae1984a832b0bc625732f55a0d4371
Prashik Ganer <prashik.ganer@gmail.com>
2024-07-26 17:21:27 +0530

    Navbar completion -- Everything working properly

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,16 +2,53 @@
 
 import React from 'react'
 import { UserButton } from '@clerk/nextjs';
+import { SearchInput } from './serach-input';
+import { OrganizationSwitcher, useOrganization } from '@clerk/nextjs';
+import { InviteButton } from './invite-button';
+
 
 
 const Navbar = () => {
+
+  const {organization} = useOrganization();
+
+
+
   return (
     <div>
-      <div className="flex items-center gap-x-4 p-5 bg-green-500">
-        <div className='hidden lg:flex lg:flex-1 bg-yellow-500'>
+      <div className="flex items-center gap-x-4 p-5 ">
+        <div className='hidden lg:flex lg:flex-1 '>
             {/* TODO: Add Search */}
-            Search
+            <SearchInput/>
         </div>
+
+        <div className='block lg:hidden flex-1'>
+        <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox:{
+              display: "flex",
+              justifyContent:"center",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: "376px"
+            },
+            organizationSwitcherTrigger:{
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent:"space-between",
+              backgroundColor: "white"
+            }
+          }
+        }}
+        />
+        </div>
+        {organization && (
+        <InviteButton/>
+        )}
         <UserButton/>
       </div>
     </div>
